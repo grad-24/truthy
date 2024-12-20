24b601f33108bb994250296a2de5580b2bfe4a67
mohammed-sada <mohammedabusada94@gmail.com>
2024-12-20 15:30:21 +0300

    disabled winston in production

--- src/config/winston.ts
+++ src/config/winston.ts
@@ -4,7 +4,7 @@ import { WinstonModuleOptions } from 'nest-winston';
 import * as WinstonCloudWatch from 'winston-cloudwatch';
 import * as config from 'config';
 
-const isProduction = process.env.NODE_ENV === 'production';
+const isProduction = false;
 const winstonConfig = config.get('winston');
 
 export default {
@@ -12,33 +12,33 @@ export default {
   exitOnError: false,
   transports: isProduction
     ? new WinstonCloudWatch({
-        name: 'Truthy CMS',
-        awsOptions: {
-          credentials: {
-            accessKeyId:
-              process.env.AWS_ACCESS_KEY || winstonConfig.awsAccessKeyId,
-            secretAccessKey:
-              process.env.AWS_KEY_SECRET || winstonConfig.awsSecretAccessKey
-          }
-        },
-        logGroupName:
-          process.env.CLOUDWATCH_GROUP_NAME || winstonConfig.groupName,
-        logStreamName:
-          process.env.CLOUDWATCH_STREAM_NAME || winstonConfig.streamName,
-        awsRegion: process.env.CLOUDWATCH_AWS_REGION || winstonConfig.awsRegion,
-        messageFormatter: function (item) {
-          return (
-            item.level + ': ' + item.message + ' ' + JSON.stringify(item.meta)
-          );
+      name: 'Truthy CMS',
+      awsOptions: {
+        credentials: {
+          accessKeyId:
+            process.env.AWS_ACCESS_KEY || winstonConfig.awsAccessKeyId,
+          secretAccessKey:
+            process.env.AWS_KEY_SECRET || winstonConfig.awsSecretAccessKey
         }
-      })
+      },
+      logGroupName:
+        process.env.CLOUDWATCH_GROUP_NAME || winstonConfig.groupName,
+      logStreamName:
+        process.env.CLOUDWATCH_STREAM_NAME || winstonConfig.streamName,
+      awsRegion: process.env.CLOUDWATCH_AWS_REGION || winstonConfig.awsRegion,
+      messageFormatter: function (item) {
+        return (
+          item.level + ': ' + item.message + ' ' + JSON.stringify(item.meta)
+        );
+      }
+    })
     : new winston.transports.Console({
-        format: winston.format.combine(
-          winston.format.timestamp(),
-          winston.format.ms(),
-          nestWinstonModuleUtilities.format.nestLike('Truthy Logger', {
-            prettyPrint: true
-          })
-        )
-      })
+      format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.ms(),
+        nestWinstonModuleUtilities.format.nestLike('Truthy Logger', {
+          prettyPrint: true
+        })
+      )
+    })
 } as WinstonModuleOptions;
